de24d1ef9b8669666f97ad6c6e072ef777dcb345
swareham-booth <swareham@chicagobooth.edu>
2021-05-06 22:14:06 +0000

    Users/username display working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,10 @@ class UsersController < ApplicationController
     render({ :template => "users_templates/index.html.erb" })
   end
 
+  def show
+    the_username = params.fetch("username")
+    @the_user = User.where({ :username => the_username })[0]
+    render({ :template => "users_templates/show.html.erb" })
+  end
+
 end
