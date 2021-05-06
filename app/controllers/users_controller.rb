51ae4d5312fd37f99c0ef8d14d0f66520e317f8a
swareham-booth <swareham@chicagobooth.edu>
2021-05-06 23:10:39 +0000

    Add users working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,12 @@ class UsersController < ApplicationController
     render({ :template => "users_templates/show.html.erb" })
   end
 
+  def create
+    input_username = params.fetch("input_username")
+    new_user = User.new 
+    new_user.username = input_username
+    new_user.save
+    redirect_to("/users/"+ new_user.username.to_s)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index" })
   get("/users", { :controller => "users", :action => "index" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
+
   get("/photos", { :controller => "photos", :action => "index" })
+
 
   get("/users/:username", { :controller => "users", :action => "show" })
   get("/photos/:photo_id", { :controller => "photos", :action => "show" })
